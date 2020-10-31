dff456c43eeeeed675c2c2239232009f4e3ff8dd
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2020-10-31 21:49:11 +0100

    [SMEP-5] generate be with spring initializer

---
+++ be/src/test/java/sperm/meets/egg/plan/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package sperm.meets.egg.plan.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

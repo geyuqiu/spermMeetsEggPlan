ad9fcb1f4fe150859281a2f28ad502f6aa829a38
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2020-11-01 20:16:48 +0100

    [SMEP-8] - add postgres docker yml - move dev config to dev.yml - default dev env

---
+++ be/README.md
@@ -0,0 +1,5 @@
+# start on prod: 
+
+     mvn  spring-boot:run -Dspring-boot.run.profiles=prod
+     
+ # 
--- be/src/main/resources/db/migration/V1__create_table.sql
+++ be/src/main/resources/db/migration/V1__create_table.sql
@@ -1,17 +1,6 @@
 --
 CREATE SEQUENCE hibernate_sequence START WITH 1000 INCREMENT BY 1;
 
-CREATE TABLE IF NOT EXISTS user
-(
-    id        int NOT NULL PRIMARY KEY,
-    name      varchar(20),
-    age       int
-
-);
-
-INSERT INTO user (id, name, age) VALUES (1, 'Yuqiu', 29);
-INSERT INTO user (id, name, age) VALUES (2, 'Yueying', 23);
-
 CREATE TABLE IF NOT EXISTS event
 (
     id        int NOT NULL PRIMARY KEY,
